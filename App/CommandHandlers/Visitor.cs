f56b4a46733321fd38dc3ed72484207e9c4cfc7e
Alexey Sokolov <sokolov.uran@yandex.ru>
2022-01-18 23:04:56 +0500

    вернул потеряный визитор

---
+++ App/CommandHandlers/Visitor.cs
@@ -0,0 +1,22 @@
+using System;
+using CommonInteraction;
+
+namespace App.CommandHandler
+{
+    public class Visitor : IVisitor<BaseCommandHandler>
+    {
+        public BaseCommandHandler Handle(KillCommandInfo info, Action<Answer, ulong, Service> send) 
+            => new KillBaseCommand(info, send);
+        public BaseCommandHandler Handle(RegCommandInfo info, Action<Answer, ulong, Service> send) 
+            => new RegPlayerBaseCommand(info, send);
+        public BaseCommandHandler Handle(ResetCommandInfo info, Action<Answer, ulong, Service> send) 
+            => new ResetGameBaseCommand(info, send);
+        public BaseCommandHandler Handle(StartCommandInfo info, Action<Answer, ulong, Service> send) 
+            => new StartBaseCommand(info, send);
+        public BaseCommandHandler Handle(VoteCommandInfo info, Action<Answer, ulong, Service> send)
+            => new VoteBaseCommand(info, send);
+
+        public BaseCommandHandler Handle(HelpCommandInfo info, Action<Answer, ulong, Service> send)
+            => new HelpBaseCommand(info, send);
+    }
+}
